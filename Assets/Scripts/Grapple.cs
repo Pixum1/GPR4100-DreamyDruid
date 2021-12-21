701082bfaa38548103007c3cf4730f53f6d1681f
Gumbok <lwerthmann@gmx.net>
2021-12-21 12:05:07 +0100

    glider-doublejump-coyotetime-grapple

---
+++ Assets/Scripts/Grapple.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+    private LineRenderer lr;
+
+    private Vector3 mousePos;
+
+    [SerializeField]
+    LayerMask grappable;
+
+    Rigidbody2D rb;
+
+    [SerializeField]
+    private float maxDistance = 50f;
+
+    private DistanceJoint2D joint;
+
+    GameObject grapplePointObj;
+
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        grapplePointObj= GameObject.Find("GrapplePoint");
+    }
+    
+    void Update()
+    {
+        CaptureMousePosition();        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGrapple();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopGrapple();
+        }        
+    }
+
+    private void LateUpdate()
+    {
+        DrawRope();
+    }
+
+    void StartGrapple()
+    {
+        RaycastHit2D hit= Physics2D.Raycast(origin: transform.position, direction: mousePos- transform.position, maxDistance, grappable);
+        if (hit)
+        {
+            
+            grapplePointObj.transform.position = hit.point;
+            grapplePointObj.transform.SetParent(hit.collider.gameObject.transform);
+
+            joint = transform.gameObject.AddComponent<DistanceJoint2D>();
+            joint.enableCollision = true;
+            joint.maxDistanceOnly = true;
+            joint.autoConfigureConnectedAnchor = false;
+            joint.autoConfigureDistance = false;
+            joint.distance = hit.distance;
+            joint.connectedBody = rb;
+
+            joint.connectedAnchor = grapplePointObj.transform.position;
+            lr.positionCount = 2;
+        }        
+    }
+
+    void StopGrapple()
+    {
+        lr.positionCount = 0;
+        Destroy(joint);
+    }
+    void DrawRope()
+    {
+        if (joint)
+        {
+            lr.SetPosition(index: 0, transform.position);
+            lr.SetPosition(index: 1, grapplePointObj.transform.position);
+        }
+    }
+
+    void CaptureMousePosition()
+    {
+        mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                        Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,73 +2,136 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour{
-    
+public class PlayerController : MonoBehaviour
+{
+
     [SerializeField]
-    private float moveSpeed = 3f; //The Players movmenet-speed
+    private float moveSpeed = 5f; //The Players movmenet-speed
+    
     [SerializeField]
     private float jumpForce = 5f;
 
     [SerializeField]
+<<<<<<< Updated upstream
     //private float maxSpeed = 5f;
+=======
+    private float maxSpeed = 8f;
+>>>>>>> Stashed changes
 
+    [SerializeField]
     private bool isGrounded; //True if the player stands on an object with 'Ground' tag
 
     private Rigidbody2D rb;
 
     private float x;
-    private float y;
 
-    private void Awake() {
+    [SerializeField]
+    private int maxJumps = 2;
+
+    private int jumpsLeft;
+
+    private Vector2 startPos;
+
+    private bool isGliding;
+
+    private void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
     }
 
 
-    void Update(){
+    void Update()
+    {
         x = Input.GetAxisRaw("Horizontal"); //Horizontal Axis
-        y = Input.GetAxisRaw("Jump"); //Vertical 'Jump' Axis
 
         //Horizontal movement
-        if(x != 0) {
+        if (x != 0 && !isGliding)
+        {
             Move();
         }
-    }
 
-    private void FixedUpdate() {
-
-        //Horizontal Movement
-        //if(x != 0) {
-        //    Move();
-        //}
-
-        //Vertical Movement
-        if (y != 0 && isGrounded) {
+        if (Input.GetButtonDown("Jump") && jumpsLeft > 0)
+        {
             Jump();
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (!isGrounded)
+            {
+                Glide();
+            }
+        }
+        else if (isGliding)
+        {
+            isGliding = false;
+        }
+
+        //reset player position
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.position = startPos;
         }
     }
 
-    private void Move() {
+
+
+    private void Move()
+    {
         //Physics based movement <-- more 'realistic' no turn mid-air
-        //if (x != 0 && rb.velocity.magnitude < maxSpeed) {
-        //    rb.AddForce(transform.right * x * moveSpeed, ForceMode2D.Force);
-        //}
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            rb.AddForce(transform.right * x * moveSpeed);
+        }
 
         //Non physics based movment <-- mid-air turns, more direct, no acceleration
-        transform.position += (transform.right * x) * Time.deltaTime * moveSpeed;
+        //transform.position += (transform.right * x) * Time.deltaTime * moveSpeed;
     }
 
-    private void Jump(){
-        rb.AddForce(transform.up * y * jumpForce, ForceMode2D.Impulse);
+    private void Jump()
+    {
+        jumpsLeft -= 1;
+        rb.velocity -= new Vector2(0, rb.velocity.y);
+        rb.AddForce(transform.up * jumpForce * 100);
     }
 
-    private void OnCollisionEnter2D(Collision2D _other) {
-        if (_other.collider.CompareTag("Ground")) {
+    private void Glide()
+    {
+        if (!isGliding)
+        {
+            isGliding = true;
+        }
+
+        float fallSpeed = Mathf.Clamp(rb.velocity.y, Mathf.NegativeInfinity, 0);
+
+        rb.AddForce(-transform.up * fallSpeed * Time.deltaTime * 75);//upforce by fallspeed
+
+        rb.AddForce(transform.up * Mathf.Abs(rb.velocity.x) * Time.deltaTime * 75);//upforce by horizontal speed
+
+        rb.AddForce(-transform.right * x * fallSpeed * Time.deltaTime * 50);//horizontal speed by fallspeed * x input
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D _other)
+    {
+        if (_other.collider.CompareTag("Ground"))
+        {
+            CancelInvoke("CoyoteTime");
+            jumpsLeft = maxJumps;
             isGrounded = true;
         }
     }
-    private void OnCollisionExit2D(Collision2D _other) {
-        if (_other.collider.CompareTag("Ground")) {
+    private void OnCollisionExit2D(Collision2D _other)
+    {
+        if (_other.collider.CompareTag("Ground"))
+        {
             isGrounded = false;
+            Invoke("CoyoteTime",0.3f);
         }
+    }
+
+    void CoyoteTime()
+    {
+        jumpsLeft = maxJumps - 1;
     }
 }
