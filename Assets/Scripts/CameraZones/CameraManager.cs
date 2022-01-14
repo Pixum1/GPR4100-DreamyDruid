b36805bec70fc5fd37695d0e8abe61c5bab14d04
Tobias Knauf <pixum2001@gmail.com>
2022-01-14 14:26:23 +0100

    test

--- Assets/Scripts/CameraZones/CameraManager.cs
+++ Assets/Scripts/CameraZones/CameraManager.cs
@@ -33,22 +33,16 @@ public class CameraManager : MonoBehaviour
     }
 
     private void Start() {
-        currentZone = zones[0];
-        previousZone = currentZone;
-
-        cam.orthographicSize = currentZone.transform.localScale.y / 2f; //adjust cam size
+        GetCurrentZone();
     }
-
     private void Update() {
-        Debug.Log(cam.orthographicSize * 2f * cam.aspect + "== " + currentZone.col.bounds.size.x);
-        
         GetCurrentZone();
 
-        if (currentZone != previousZone) {
+        if (currentZone != previousZone || previousZone == null) {
             SetCameraPosition(); //set camera position if the current zone has been switched (saves performance)
         }
 
-        if(cam.orthographicSize * 2f * cam.aspect < currentZone.col.bounds.size.x) {
+        if(cameraWidth < currentZone.col.bounds.size.x) {
             if (CheckCameraBounds(currentZone)) {
                 cam.transform.position = new Vector3(player.transform.position.x, currentZone.transform.position.y, cam.transform.position.z);
                 AdjustCamEdge(currentZone);
@@ -86,7 +80,6 @@ public class CameraManager : MonoBehaviour
         camCorners[1] = leftLowerCorner;
         camCorners[2] = rightUpperCorner;
         camCorners[3] = rightLowerCorner;
-
         return camCorners;
     }
 
@@ -101,19 +94,29 @@ public class CameraManager : MonoBehaviour
             Vector3 newPos = _zone.col.bounds.min + new Vector3(cameraWidth / 2, 0, 0); //center of the box on the left most edge + half the camera's width
             cam.transform.position = new Vector3(newPos.x, cam.transform.position.y, cam.transform.position.z);
             return;
-        }
-        //right edge Check
-        else if(!_zone.col.bounds.Contains(GetCameraBounds(_zone)[2]) && !_zone.col.bounds.Contains(GetCameraBounds(_zone)[3])) {
+        }//right edge Check
+        else if (!_zone.col.bounds.Contains(GetCameraBounds(_zone)[2]) && !_zone.col.bounds.Contains(GetCameraBounds(_zone)[3])) {
             Vector3 newPos = _zone.col.bounds.max - new Vector3(cameraWidth / 2, 0, 0); //center of the box on the right most edge - half of the camera's width
             cam.transform.position = new Vector3(newPos.x, cam.transform.position.y, cam.transform.position.z);
             return;
         }
+        ////Bottom check
+        //else if(!_zone.col.bounds.Contains(GetCameraBounds(_zone)[1]) && !_zone.col.bounds.Contains(GetCameraBounds(_zone)[3])) {
+        //    Vector3 newPos = (new Vector3(_zone.col.bounds.center.x, _zone.col.bounds.center.y - _zone.col.bounds.extents.y, 0)) - new Vector3(cameraWidth / 2, 0, 0);
+        //    cam.transform.position = new Vector3(cam.transform.position.x, newPos.y, cam.transform.position.z);
+        //}
+        ////Top Check
+        //else if(!_zone.col.bounds.Contains(GetCameraBounds(_zone)[0]) && !_zone.col.bounds.Contains(GetCameraBounds(_zone)[2])) {
+        //    Vector3 newPos = (new Vector3(_zone.col.bounds.center.x, _zone.col.bounds.center.y + _zone.col.bounds.extents.y, 0)) - new Vector3(cameraWidth / 2, 0, 0);
+        //    cam.transform.position = new Vector3(cam.transform.position.x, newPos.y, cam.transform.position.z);
+        //}
     }
 
     /// <summary>
     /// Smoothly transition the cameras position to the current zone and adjust its size accordingly.
     /// </summary>
     private void SetCameraPosition() {
+        Debug.Log("Setting Camera Position");
         Time.timeScale = 0f;
 
         Vector3 newPos;
@@ -124,16 +127,16 @@ public class CameraManager : MonoBehaviour
         if (cameraWidth < currentZone.col.bounds.size.x) {
 
             //if player is on the right side of the CameraZone
-            if(player.transform.position.x >= currentZone.col.bounds.max.x) {
+            if(player.transform.position.x > currentZone.col.bounds.max.x) {
                 Vector3 side = currentZone.col.bounds.min + new Vector3(cameraWidth / 2, 0, 0);
 
-                newPos = new Vector3(side.x, cam.transform.position.y, cam.transform.position.z);
+                newPos = new Vector3(side.x, currentZone.transform.position.y, cam.transform.position.z);
             }
             //if player is on the left side of the CameraZone
             else {
                 Vector3 side = currentZone.col.bounds.max - new Vector3(cameraWidth / 2, 0, 0);
 
-                newPos = new Vector3(side.x, cam.transform.position.y, cam.transform.position.z);
+                newPos = new Vector3(side.x, currentZone.transform.position.y, cam.transform.position.z);
             }
             cam.transform.position = Vector3.Lerp(cam.transform.position, newPos, camSwitchSpeed * 0.02f);
 
