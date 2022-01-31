2b402d998994aab62e6ac39a642daeda182844dd
Tobias Knauf <pixum2001@gmail.com>
2022-01-31 15:30:27 +0100

    WallHang & WallJump animation
    added the animations for wall hanging and an animation for when you look in the opposite direction of that wall

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -8,6 +8,10 @@ public class PlayerAnimation : MonoBehaviour {
 
     [SerializeField]
     private SpriteRenderer playerSprite;
+    [SerializeField]
+    private Sprite wallHangSprite;
+    [SerializeField]
+    private Sprite wallHangLookAwaySprite;
 
     [SerializeField]
     private Animator animator;
@@ -17,30 +21,41 @@ public class PlayerAnimation : MonoBehaviour {
     const string runAnim = "PlayerRun_anim";
     const string jumpAnim = "PlayerJump_anim";
     const string fallAnim = "PlayerFalling_anim";
+    const string wallAnim = "PlayerOnWall_anim";
+    const string wallLookAnim = "PlayerOnWallLookAway_anim";
 
     private bool jumping { get { return !player.pCollision.m_IsGrounded && player.rb.velocity.y > 0f; } }
     private bool falling { get { return !player.pCollision.m_IsGrounded && !jumping; } }
     private bool running { get { return player.pMovement.m_CanMove && (!jumping && !falling); } }
     private bool idle { get { return !running && !jumping && !falling; } }
+    private bool wallHang { get { return player.pMovement.m_CanWallHang; } }
 
     private void Awake() {
         animator = GetComponent<Animator>();
     }
 
     void Update() {
-        ApplyRotation();
+        if (!wallHang) {
+            ApplyRotation();
 
-        if (jumping) {
-            ChangeAnimationState(jumpAnim);
+            if (jumping)
+                ChangeAnimationState(jumpAnim);
+
+            if (falling)
+                ChangeAnimationState(fallAnim);
+
+            if (running && !jumping)
+                ChangeAnimationState(runAnim);
+
+            if (idle)
+                ChangeAnimationState(idleAnim);
         }
-        if (falling) {
-            ChangeAnimationState(fallAnim);
-        }
-        if (running && !jumping) {
-            ChangeAnimationState(runAnim);
-        }
-        if (idle) {
-            ChangeAnimationState(idleAnim);
+
+        if (wallHang) {
+            if ((player.pCollision.m_IsOnLeftWall && player.pMovement.m_HorizontalDir > 0f) || (player.pCollision.m_IsOnRightWall && player.pMovement.m_HorizontalDir < 0f))
+                ChangeAnimationState(wallLookAnim);
+            else
+                ChangeAnimationState(wallAnim);
         }
     }
 
