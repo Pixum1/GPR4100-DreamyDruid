02840e02b3b6f9391c198ffb398f04df6e2274ca
Tobias Knauf <pixum2001@gmail.com>
2022-01-31 15:03:59 +0100

    Basic Animations
    Added the animation & transitions for idle, running, jumping and falling

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -9,8 +9,39 @@ public class PlayerAnimation : MonoBehaviour {
     [SerializeField]
     private SpriteRenderer playerSprite;
 
+    [SerializeField]
+    private Animator animator;
+    private string currentAnimation;
+
+    const string idleAnim = "PlayerIdle_anim";
+    const string runAnim = "PlayerRun_anim";
+    const string jumpAnim = "PlayerJump_anim";
+    const string fallAnim = "PlayerFalling_anim";
+
+    private bool jumping { get { return !player.pCollision.m_IsGrounded && player.rb.velocity.y > 0f; } }
+    private bool falling { get { return !player.pCollision.m_IsGrounded && !jumping; } }
+    private bool running { get { return player.pMovement.m_CanMove && (!jumping && !falling); } }
+    private bool idle { get { return !running && !jumping && !falling; } }
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
     void Update() {
         ApplyRotation();
+
+        if (jumping) {
+            ChangeAnimationState(jumpAnim);
+        }
+        if (falling) {
+            ChangeAnimationState(fallAnim);
+        }
+        if (running && !jumping) {
+            ChangeAnimationState(runAnim);
+        }
+        if (idle) {
+            ChangeAnimationState(idleAnim);
+        }
     }
 
     private void ApplyRotation() {
@@ -20,5 +51,12 @@ public class PlayerAnimation : MonoBehaviour {
         else if (player.pMovement.m_HorizontalDir < 0) {
             playerSprite.flipX = true;
         }
+    }
+
+    private void ChangeAnimationState(string _animationName) {
+        if (currentAnimation == _animationName) return;
+
+        animator.Play(_animationName);
+        currentAnimation = _animationName;
     }
 }
