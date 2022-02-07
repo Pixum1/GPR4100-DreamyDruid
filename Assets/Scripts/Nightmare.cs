53a7b356a472f51f64aefe0e85cc9edadeade44a
Gumbok <lwerthmann@gmx.net>
2022-02-07 17:13:58 +0100

    Revert "Nightmare rework"
    This reverts commit c913c684a425da3a3b722d7eb75347a4d70d2201.

--- Assets/Scripts/Nightmare.cs
+++ Assets/Scripts/Nightmare.cs
@@ -2,83 +2,105 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 using UnityEngine.Tilemaps;
 
 public class Nightmare : MonoBehaviour
 {
     private PlayerController playerController;
+
     Transform player;
+
     [SerializeField]
     float maxTime;
+
     Checkpoint currentCheckpoint;
     CheckpointManager cPManager;
-    Vector3Int resetPosition;
+
     [SerializeField]
-    Vector3Int startPosition;
+    List<Vector3Int> emptyNightmareTiles = new List<Vector3Int>();
+    [SerializeField]
+    List<Vector3Int> usedNightmareTiles = new List<Vector3Int>();
+    [SerializeField]
+    Vector3Int startPos;
+    [SerializeField]
+    Vector3Int resetPos;
+
+    int i;
+
     [SerializeField]
     Tilemap nightmareTilemap;
-    [SerializeField]
-    Tilemap groundTilemap;
+
     [SerializeField]
     TileBase nmTile;
-    List<Vector3> playerPathPoints = new List<Vector3>();
+
+    TileInfoTool tIT;
+
     private void Start()
     {
         playerController = FindObjectOfType<PlayerController>();
+        tIT = GetComponent<TileInfoTool>();
         cPManager = GameObject.Find("CheckPointManager").GetComponent<CheckpointManager>();
         playerController.pHealth.e_PlayerDied += new Action(ResetNightmare);
+        emptyNightmareTiles = tIT.LoadTileData().surroundingTilesData;
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        StartCoroutine(GetPathPoints());
-        StartCoroutine(GetSurroundingTiles(startPosition));
+        usedNightmareTiles.Add(startPos);
+        emptyNightmareTiles.Remove(startPos);
+        GetNextTile();
+        
     }
-    private void ResetNightmare()
+
+    void GetNextTile()
     {
-        StopAllCoroutines();
-        nightmareTilemap.ClearAllTiles();
-        currentCheckpoint = cPManager.currentCP;
-        resetPosition = currentCheckpoint.nightmareResetPosition;
-        StartCoroutine(GetPathPoints());
-        StartCoroutine(GetSurroundingTiles(resetPosition));
-    }
-    private IEnumerator GetPathPoints()
-    {
-        playerPathPoints.Insert(0, player.position);
-        if (playerPathPoints.Count >= 11)
+        StartCoroutine(GetSurroundingTiles(usedNightmareTiles[i]));
+        if (i < usedNightmareTiles.Count - 1)
         {
-            playerPathPoints.RemoveAt(10);
+            i++;
         }
-        yield return new WaitForSeconds(0.3f);
-        StartCoroutine(GetPathPoints());
     }
+
     private IEnumerator GetSurroundingTiles(Vector3Int _tile)
     {
-        for (int x = 1; x > -2; x--)
+        List<Vector3Int> bufferTiles = new List<Vector3Int>();
+        float dist = Vector3.Distance(_tile, player.position);
+        for (int x = -1; x < 2; x++)
         {
             for (int y = -1; y < 2; y++)
             {
                 Vector3Int offset = new Vector3Int(x, y, 0);
                 Vector3Int pos = _tile + offset;
-                if (!groundTilemap.HasTile(pos) && !nightmareTilemap.HasTile(pos))
+
+                if (emptyNightmareTiles.Contains(pos)&&dist<50)
                 {
-                    Vector2 posVec2 = new Vector2(pos.x, pos.y);
-                    float distanceToPlayer = Vector2.Distance(player.position, posVec2);
-                    float f = Mathf.Max(0.002f * Mathf.Pow(distanceToPlayer, 2.0f), 1);
-                    float timeToNext = maxTime / f;
-                    for (int i = 0; i < playerPathPoints.Count; i++)
-                    {
-                        float distanceToPathpoint = Vector2.Distance(playerPathPoints[i], posVec2);
-                        if (distanceToPathpoint < 20)
-                        {
-                            nightmareTilemap.SetTile(pos, nmTile);
-                            yield return new WaitForSeconds(timeToNext * UnityEngine.Random.Range(1, 1.1f));
-                            StartCoroutine(GetSurroundingTiles(pos));
-                            break;
-                        }
-                    }
+                    bufferTiles.Add(pos);
+                    emptyNightmareTiles.Remove(pos);
                 }
             }
         }
+        float f = Mathf.Max(0.00075f * Mathf.Pow(dist, 2.8f), 1);
+        float timeToNext = maxTime / f;
+        yield return new WaitForSeconds(timeToNext);
+        usedNightmareTiles.AddRange(bufferTiles);
+        
+        nightmareTilemap.SetTile(_tile,nmTile);
+        GetNextTile();
     }
+
+    private void ResetNightmare()
+    {
+        StopAllCoroutines();
+        nightmareTilemap.ClearAllTiles();
+        usedNightmareTiles.Clear();
+        emptyNightmareTiles.Clear();
+        emptyNightmareTiles = tIT.LoadTileData().surroundingTilesData;
+        currentCheckpoint = cPManager.currentCP;
+        resetPos = currentCheckpoint.nightmarePosWhenCollected;
+        usedNightmareTiles.Add(resetPos);
+        emptyNightmareTiles.Remove(resetPos);
+        i = 0;
+        GetNextTile();
+    }
+
     private void OnDisable()
     {
         playerController.pHealth.e_PlayerDied -= ResetNightmare;
