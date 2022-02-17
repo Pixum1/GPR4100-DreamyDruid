df4dc6e4b1182058435ac5e1582116a72c0fe867
TobiasK <pixum2001@gmail.com>
2022-02-17 14:07:43 +0100

    parallax and background color

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -11,13 +11,38 @@ public class Parallax : MonoBehaviour
     [SerializeField]
     private float autoScrollEffect;
 
+    private SpriteRenderer sp;
+    private Nightmare nm;
+    [SerializeField]
+    private Color nmColor;
+    [SerializeField]
+    private float colorChangeSpeed;
+
     private void Start()
     {
+        nm = FindObjectOfType<Nightmare>();
+        sp=GetComponent<SpriteRenderer>();
         startPos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
     private void Update()
     {
+        if (nm.active)
+        {
+            if (sp.color.g >= nmColor.g && sp.color.b >= nmColor.b)
+            {
+                sp.color = new Color(sp.color.r, sp.color.g - (Time.deltaTime * colorChangeSpeed), sp.color.b - (Time.deltaTime * colorChangeSpeed));
+            }
+            foreach (Transform child in transform)
+            {
+                SpriteRenderer childSp = child.GetComponent<SpriteRenderer>();
+                if (childSp.color.g >= nmColor.g && childSp.color.b >= nmColor.b)
+                {
+                    childSp.color = new Color(childSp.color.r, childSp.color.g - (Time.deltaTime * colorChangeSpeed), childSp.color.b - (Time.deltaTime * colorChangeSpeed));
+                }
+            }
+        }
+
         float temp = Camera.main.transform.position.x * (1 - parallaxEffect);
         float distance = Camera.main.transform.position.x * parallaxEffect;
 
@@ -33,8 +58,11 @@ public class Parallax : MonoBehaviour
             {
                 transform.position = new Vector2(startPos, transform.position.y);
             }
+            else if(transform.position.x < startPos - length)
+            {
+                transform.position = new Vector2(startPos, transform.position.y);
+            }
         }
-
         if (temp > startPos + length)
         {
             startPos += length;
