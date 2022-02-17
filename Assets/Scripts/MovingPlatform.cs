36565c3e0d9bb0aa01457ab6f5318d7e7b18b134
Gumbok <lwerthmann@gmx.net>
2022-02-17 16:51:32 +0100

    Platform Update

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -12,6 +12,7 @@ public class MovingPlatform : MonoBehaviour
     private Transform rightWaypoint;
     [SerializeField]
     private float speed;
+    PlayerCollision playerCollision;
 
     private Vector2 destLeftPos;
     private Vector2 destRightPos;
@@ -19,24 +20,30 @@ public class MovingPlatform : MonoBehaviour
     private bool movingLeft;
     private bool movingRight;
 
-    private void Awake() {
+    private void Awake()
+    {
         destLeftPos = new Vector2(leftWaypoint.position.x + platform.GetComponent<Collider2D>().bounds.extents.x, leftWaypoint.position.y);
         destRightPos = new Vector2(rightWaypoint.position.x - platform.GetComponent<Collider2D>().bounds.extents.x, rightWaypoint.position.y);
     }
 
-    private void Start() {
+    private void Start()
+    {
+        playerCollision = FindObjectOfType<PlayerCollision>();
         StartCoroutine(Move());
     }
 
-    private IEnumerator Move() {
-        while(Vector2.Distance(platform.position, destLeftPos) > 0) {
+    private IEnumerator Move()
+    {
+        while (Vector2.Distance(platform.position, destLeftPos) > 0)
+        {
             movingLeft = true;
             movingRight = false;
             platform.position = Vector2.MoveTowards(platform.position, destLeftPos, speed * Time.deltaTime);
             yield return null;
         }
-        while(Vector2.Distance(platform.position, destRightPos) > 0) {
-            movingLeft=false;
+        while (Vector2.Distance(platform.position, destRightPos) > 0)
+        {
+            movingLeft = false;
             movingRight = true;
             platform.position = Vector2.MoveTowards(platform.position, destRightPos, speed * Time.deltaTime);
             yield return null;
@@ -44,11 +51,19 @@ public class MovingPlatform : MonoBehaviour
         StartCoroutine(Move());
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("Player") && playerCollision.m_IsGrounded)
         {
-            collision.collider.transform.SetParent(transform);
+            if (collision.collider.transform.parent == transform)
+            {
+                CancelInvoke("Unparent");
+            }
+            else
+            {
+                collision.collider.transform.SetParent(transform);
+                collision.collider.attachedRigidbody.AddForce(Vector2.up * 8, ForceMode2D.Impulse);                
+            }
         }
     }
 
@@ -56,11 +71,17 @@ public class MovingPlatform : MonoBehaviour
     {
         if (collision.collider.CompareTag("Player"))
         {
-            collision.collider.transform.SetParent(null);
+            Invoke("Unparent",0.2f);
         }
     }
 
-    private void OnDrawGizmos() {
+    void Unparent()
+    {
+        playerCollision.transform.SetParent(null);
+    }
+
+    private void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(leftWaypoint.position, leftWaypoint.localScale);
         Gizmos.DrawWireCube(rightWaypoint.position, rightWaypoint.localScale);
